51936ae3545ed4237d1989595741749621ac94fb
Vikas Sharma34 <Vikas_Sharma34@ad.infosys.com>
2017-12-27 12:09:34 +0530

    New Addition Project created

---
+++ ConsoleAdditionProj/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAdditionProj
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            Console.WriteLine("The Output for Addition is :" + prog.Addition(2, 7));
+            Console.ReadLine();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private int Addition(int a,int b)
+        {
+            return a + b;
+        }
+    }
+
+
+
+}
